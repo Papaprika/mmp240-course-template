87470ceeca3941981c5bce0196587d995d6f1513
Papaprika <121039809+Papaprika@users.noreply.github.com>
2024-08-29 03:47:00 +0000

    Mid and Final

---
+++ Final/Catalogue/script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  document.getElementById("fademe").style.opacity = 1;
+  document.getElementById("fadem").style.opacity = 1;
+  document.getElementById("gas").style.opacity = 1;
+  };
+  
