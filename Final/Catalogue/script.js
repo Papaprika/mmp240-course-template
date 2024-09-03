a0dc18d3092db89088a8d7a77ce78d3acc0b23c0
Papaprika <121039809+Papaprika@users.noreply.github.com>
2024-09-03 07:05:43 +0000

    Ugh

--- Final/Catalogue/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-  document.getElementById("fademe").style.opacity = 1;
-  document.getElementById("fadem").style.opacity = 1;
-  document.getElementById("gas").style.opacity = 1;
-  };
-  
